aec4f065bb62af6212ef5a7edb460e6db398282a
RJ <rjjor23@gmail.com>
2021-10-07 08:41:52 -0700

    Function singleRound() complete

--- script.js
+++ script.js
@@ -13,9 +13,43 @@ function computerPlay() {
 
 // input the necessary arguments to determine a victor in RPS
 function singleRound(playerSelection, computerSelection) {
+    // local variables
+    let playerSelection2 = playerSelection.toLowerCase();
+    let computerSelection2 = computerSelection.toLowerCase();
+    let victor = '';
 
-}
+    // logic
+    
+    if (//human wins
+        (playerSelection2 == 'rock' && computerSelection2 == 'scissors') ||
+        (playerSelection2 == 'paper' && computerSelection2 == 'rock') ||
+        (playerSelection2 == 'scissors' && computerSelection2 == 'paper')) {
+        let victor = 'Human wins!!!';
+        return victor;
+        // console.log('Human wins!!!');
+    } else if (//computer wins
+        (playerSelection2 == 'scissors' && computerSelection2 == 'rock') ||
+        (playerSelection2 == 'rock' && computerSelection2 == 'paper') ||
+        (playerSelection2 == 'paper' && computerSelection2 == 'scissors')) {
+            let victor = 'Computer wins!!!';
+            return victor;
+        // console.log('Computer wins!!!');
+    } else if (
+        (playerSelection2 == 'scissors' && computerSelection2 == 'scissors') ||
+        (playerSelection2 == 'rock' && computerSelection2 == 'rock') ||
+        (playerSelection2 == 'paper' && computerSelection2 == 'paper')
+    ) {//tie
+        let victor = 'Tie!!!';
+        return victor;
+        // console.log('Tie!!!');
+    } else {//invalid input
+        let victor = 'Please enter valid values, thanks.';
+        return victor;
+        // console.log('please enter valid values');
+    }// end if
+    
+}// end singleRound() 
 
-
+console.log(singleRound('fish', 'centipede'));
 
 
