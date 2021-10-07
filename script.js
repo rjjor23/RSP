74c636b53342b9cf04742e432fc2108a99c7b8fc
RJ <rjjor23@gmail.com>
2021-10-07 07:53:22 -0700

    Shells out the singleRound function - good place to restart

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// console.log("Systems Check...");
-let res ='a';
+// Rock, Scissors, Paper
+// Project from the OP boot camp
+// 20211006 
 
+// Returns Rock | Paper | Scissors randomly
 function computerPlay() {
     var list = ["rock", "scissors", "paper"];
     let res = list[Math.floor(Math.random()*list.length)];
+    return res;
     console.log(res);
 }
+// computerPlay();
 
-computerPlay();
+// input the necessary arguments to determine a victor in RPS
+function singleRound(playerSelection, computerSelection) {
+
+}
+
+
+
 
